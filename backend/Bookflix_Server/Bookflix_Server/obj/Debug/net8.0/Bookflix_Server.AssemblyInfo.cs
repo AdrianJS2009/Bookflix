74cabd3bdc5170822bb4ef6b6219b33a8d1c04c9
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-10-24 17:53:30 +0200

    Añadida estructura de carpetas
    Se ha creado las carpetas que almacenaran la logica de nuestro backend para tenerlo más organizado

--- backend/Bookflix_Server/Bookflix_Server/obj/Debug/net8.0/Bookflix_Server.AssemblyInfo.cs
+++ backend/Bookflix_Server/Bookflix_Server/obj/Debug/net8.0/Bookflix_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookflix_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34be548ff4be7edcefaf1c761155379b814bff57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5ffda76310da43763fa5250fd4f41d201faffd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookflix_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookflix_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
