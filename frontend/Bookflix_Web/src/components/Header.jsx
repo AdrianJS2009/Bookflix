6b13908ce99e87e52b5f605594d1bab6f740565f
Elias Robles <149778129+erobrui2812@users.noreply.github.com>
2024-10-25 17:04:54 +0200

    Importacion de Header de HTML a React Vite

---
+++ frontend/Bookflix_Web/src/components/Header.jsx
@@ -0,0 +1,37 @@
+// src/components/Header.jsx
+import React from 'react';
+import '../styles/default.css';
+import '../styles/header.css';
+import '../styles/footer.css';
+import '../styles/styles.css';
+
+const Header = () => {
+  return (
+    <header className="header fondo-negro">
+      <div className="nav-logo">
+        <a href="#">
+          <img src="/assets/logos/Logo-degradado-con bisel.png" className="logo-navbar" alt="Bookflix logo" />
+        </a>
+        <div className="user-cart texto-pequeño-bold">
+          <a href="#">
+            <img src="/assets/iconos/Logo Usuario.png" className="iconos" alt="" /> Usuario
+          </a>
+          <a href="#">
+            <img src="/assets/iconos/Logo Cesta.png" className="iconos" alt="" /> Cesta
+          </a>
+        </div>
+      </div>
+      <nav className="nav-principal fondo-verde">
+        <ul className="texto-pequeño-bold texto-negro">
+          <li><a href="#">Novedades</a></li>
+          <li><a href="#">Géneros</a></li>
+          <li><a href="#">Top ventas</a></li>
+          <li><a href="#">Bundles</a></li>
+          <li><a href="#">Catálogo</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
