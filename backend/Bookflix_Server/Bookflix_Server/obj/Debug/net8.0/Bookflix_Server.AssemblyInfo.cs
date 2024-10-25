52ecc278bec88598196c6f2f7f49f93c2323318f
Gio <ggiomes590@g.educaand.es>
2024-10-25 16:27:49 +0200

    Carpetas y clases creadas

--- backend/Bookflix_Server/Bookflix_Server/obj/Debug/net8.0/Bookflix_Server.AssemblyInfo.cs
+++ backend/Bookflix_Server/Bookflix_Server/obj/Debug/net8.0/Bookflix_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookflix_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5ffda76310da43763fa5250fd4f41d201faffd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74cabd3bdc5170822bb4ef6b6219b33a8d1c04c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookflix_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookflix_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
