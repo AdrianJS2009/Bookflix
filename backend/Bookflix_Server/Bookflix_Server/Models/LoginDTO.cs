9864a65cfa90c0bf54f19dd54504f7519c878c2c
Gio <ggiomes590@g.educaand.es>
2024-10-28 18:39:54 +0100

    LoginDTO creada

---
+++ backend/Bookflix_Server/Bookflix_Server/Models/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookflix_Server.Models
+{
+    // Clase DTO que representa los datos de login enviados desde el cliente
+    public class LoginDto
+    {
+        public string UserName { get; set; } // Nombre de usuario o email
+
+        public string Password { get; set; } // Contraseña del usuario
+    }
+}
