bf1b1207336f77c219bd24891d896b933343f2c2
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-10-28 18:26:00 +0100

    creado la interfaz de userrepository

---
+++ backend/Bookflix_Server/Bookflix_Server/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookflix_Server.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(int id); // Método para obtener un usuario por ID
+        Task AddAsync(User user); // Método para añadir un nuevo usuario
+        Task<bool> UserExistsAsync(string email); // Verifica si un usuario existe via email
+    }
+}
