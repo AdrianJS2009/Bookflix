6b04503114d4dcf0830f98b81c868205ea9a98fd
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-10-28 18:48:38 +0100

    Terminado el AuthController
    La clase gorda de la bbdd, controlador del token por usuario

--- backend/Bookflix_Server/Bookflix_Server/Controllers/AuthController.cs
+++ backend/Bookflix_Server/Bookflix_Server/Controllers/AuthController.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Bookflix_Server.Models;
+using Bookflix_Server.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Bookflix_Server.Controllers
 {
-    internal class AuthController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork; // UOW para entrar en los repos
+        private readonly IConfiguration _config; // Config para utilizar claves JWT
+
+        public AuthController(IUnitOfWork unitOfWork, IConfiguration config)
+        {
+            _unitOfWork = unitOfWork;
+            _config = config;
+        }
+
+        // Endpoint
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] LoginDto model)
+        {
+            // Comprobar el user en la BBDD
+            var userExists = await _unitOfWork.Users.UserExistsAsync(model.UserName);
+            if (!userExists || model.Password != "contraseña")
+            {
+                return Unauthorized(); // 
+            }
+
+            // Crear el token para el user
+            var token = GenerateToken(model.UserName);
+            return Ok(token);
+        }
+
+        // Genera el token JWT basado en las claims del usuario
+        private string GenerateToken(string userName)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userName), // Claim de nombre de usuario
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // ID único del token
+                new Claim(ClaimTypes.Name, userName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(700),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
