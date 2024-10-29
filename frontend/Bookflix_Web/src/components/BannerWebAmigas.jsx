4cf26f63debd20519a759428ea23f6e7470d2aab
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-10-29 22:51:43 +0100

    Eliminado jsx innecesarios

--- frontend/Bookflix_Web/src/components/BannerWebAmigas.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BannerWebAmigas(){
-  return (
-    <div className='box-banner'>
-      <img src='/assets/banner/BannerWebAmigas.png' alt="Banner Web Amigas" />
-    </div>
-  );
-}
