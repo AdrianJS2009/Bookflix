e2406d3ea7736ac553505c053a788f798e3cdbf5
Elias Robles <149778129+erobrui2812@users.noreply.github.com>
2024-10-30 16:05:11 +0100

    Prueba de login con username en el header

--- frontend/Bookflix_Web/src/components/Header.jsx
+++ frontend/Bookflix_Web/src/components/Header.jsx
@@ -1,33 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/default.css";
 import "../styles/header.css";
 
 const Header = () => {
+  const [username, setUsername] = useState("");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const storedToken = localStorage.getItem("token");
+      
+      if (storedToken) {
+        setToken(storedToken);
+        const response = await fetch("http://localhost:5000/api/user/info", {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        });
+        const data = await response.json();
+        setUsername(data.username);
+      }
+      
+    };
+
+    fetchToken();
+  }, []);
+
   return (
     <header className="header fondo-negro">
       <div className="nav-logo">
         <Link to="/">
           <img
-            src="/assets/logos/Logo-degradado-con bisel.png"
+            src="/assets/logos/Logo-degradado-con-bisel.png"
             className="logo-navbar"
             alt="Bookflix logo"
           />
         </Link>
         <div className="user-cart texto-pequeño-bold">
-          <Link to="/login">
-            <img
-              src="/assets/iconos/Logo Usuario.png"
-              className="iconos"
-              alt=""
-            />{" "}
-            Usuario
-          </Link>
+          {token ? (
+            <Link to="/profile">
+              <img
+                src="/assets/iconos/Logo-Usuario.png"
+                className="iconos"
+                alt="User Icon"
+              />{" "}
+              Hola {username}
+            </Link>
+          ) : (
+            <Link to="/login">
+              <img
+                src="/assets/iconos/Logo-Usuario.png"
+                className="iconos"
+                alt="User Icon"
+              />{" "}
+              Usuario
+            </Link>
+          )}
+          
           <Link to="/carrito">
             <img
-              src="/assets/iconos/Logo Cesta.png"
+              src="/assets/iconos/Logo-Cesta.png"
               className="iconos"
-              alt=""
+              alt="Shopping Cart Icon"
             />{" "}
             Cesta
           </Link>
