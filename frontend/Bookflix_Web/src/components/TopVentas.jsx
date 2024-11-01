4f6352c16407f526631109976182854956107bba
Gio <ggiomes590@g.educaand.es>
2024-11-01 21:10:00 +0100

    topventas cambiado a navlink

--- frontend/Bookflix_Web/src/components/TopVentas.jsx
+++ frontend/Bookflix_Web/src/components/TopVentas.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./styles/Topventas.module.css";
 
-export default function TopVentas(){
+export default function TopVentas() {
   return (
     <section id="top-ventas" className={classes.topVentasSection}>
       <div className="libro">
-          <img src="/assets/libros/1.png" className={classes.libroImagen} />
-          <img src="/assets/numeros/1.png" className={classes.puestoImagen} />
+        <NavLink to="/libro/1">
+          <img src="/assets/libros/1.png" className={classes.libroImagen} alt="Libro 1" />
+        </NavLink>
+        <img src="/assets/numeros/1.png" className={classes.puestoImagen} alt="Puesto 1" />
       </div>
       <div className="libro">
-          <img src="/assets/libros/2.png" className={classes.libroImagen} />
-          <img src="/assets/numeros/2.png" className={classes.puestoImagen} />
+        <NavLink to="/libro/2">
+          <img src="/assets/libros/2.png" className={classes.libroImagen} alt="Libro 2" />
+        </NavLink>
+        <img src="/assets/numeros/2.png" className={classes.puestoImagen} alt="Puesto 2" />
       </div>
       <div className="libro">
-          <img src="/assets/libros/3.png" className={classes.libroImagen} />
-          <img src="/assets/numeros/3.png" className={classes.puestoImagen} />
+        <NavLink to="/libro/3">
+          <img src="/assets/libros/3.png" className={classes.libroImagen} alt="Libro 3" />
+        </NavLink>
+        <img src="/assets/numeros/3.png" className={classes.puestoImagen} alt="Puesto 3" />
       </div>
     </section>
   );
