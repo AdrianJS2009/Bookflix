820011e9232c868cac84683c95823935b32ec0f0
Gio <ggiomes590@g.educaand.es>
2024-11-01 20:52:25 +0100

    generos.jsx cambiado a navlink

--- frontend/Bookflix_Web/src/components/Generos.jsx
+++ frontend/Bookflix_Web/src/components/Generos.jsx
@@ -1,63 +1,65 @@
 import "../styles/default.css";
 import classes from "./styles/Generos.module.css";
+import { NavLink } from 'react-router-dom';
 
 const Generos = () => {
   return (
     <section id="generos" href="generos">
       <div className={classes.generosContainer}>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        <NavLink to="/ciencia-ficcion" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.cienciaFiccion}`}>
             <p>Ciencia Ficción</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        </NavLink>
+        <NavLink to="/fantasia" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.fantasia}`}>
             <p>Fantasía</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        </NavLink>
+        <NavLink to="/terror" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.terror}`}>
             <p>Terror</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        </NavLink>
+        <NavLink to="/misterio" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.misterio}`}>
             <p>Misterio</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        </NavLink>
+        <NavLink to="/thriller" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.thriller}`}>
             <p>Thriller</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        </NavLink>
+        <NavLink to="/romance" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.romance}`}>
             <p>Romance</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        </NavLink>
+        <NavLink to="/biografia" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.biografia}`}>
             <p>Biografía</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
+        </NavLink>
+        <NavLink to="/historicos" className="texto-mediano-bold texto-blanco" activeClassName="active">
           <div className={`${classes.genero} ${classes.historicos}`}>
             <p>Históricos</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
-          <div className={`${classes.genero} ${classes.cinefilos}`}>
+        </NavLink>
+        <NavLink to="/historicos" className="texto-mediano-bold texto-blanco" activeClassName="active">
+          <div className={`${classes.genero} ${classes.historicos}`}>
             <p>Cinéfilos</p>
           </div>
-        </a>
-        <a href="" className="texto-mediano-bold texto-blanco">
-          <div className={`${classes.genero} ${classes.novelas}`}>
+        </NavLink>
+        <NavLink to="/historicos" className="texto-mediano-bold texto-blanco" activeClassName="active">
+          <div className={`${classes.genero} ${classes.historicos}`}>
             <p>Novelas</p>
           </div>
-        </a>
+        </NavLink>
       </div>
     </section>
   );
-};
+}
 
 export default Generos;
+
