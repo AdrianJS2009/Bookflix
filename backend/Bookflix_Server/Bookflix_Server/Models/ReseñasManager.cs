1cf89dba03d29041707aea3d5c106e5151557fb6
Gio <ggiomes590@g.educaand.es>
2024-11-04 19:54:28 +0100

    tabla reseña y reseñasmanager separadas en diferentes clases

--- backend/Bookflix_Server/Bookflix_Server/Models/ReseñasManager.cs
+++ backend/Bookflix_Server/Bookflix_Server/Models/ReseñasManager.cs
@@ -4,18 +4,8 @@ using System.Linq;
 
 namespace Bookflix_Server.Models;
 
-// Clase que representa una reseña individual
-internal class Reseña
-{
-    // Propiedades de la reseña
-    public string Autor { get; set; } // Nombre del autor de la reseña
-    public string Texto { get; set; } // Texto de la reseña
-    public int Estrellas { get; set; } // Número de estrellas otorgadas en la reseña
-    public DateTime FechaPublicacion { get; set; } // Fecha de publicación de la reseña
-}
-
 // Clase que maneja una colección de reseñas
-internal class Reseñas
+internal class ReseñasManager
 {
     // Lista privada que almacena las reseñas
     private List<Reseña> listaReseñas = new List<Reseña>();
