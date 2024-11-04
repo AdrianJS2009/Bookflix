761c95cd60065232965153391c6b2661e863a590
Gio <ggiomes590@g.educaand.es>
2024-11-04 19:36:16 +0100

    clase reseña creada, y clase interna que almacena cada reseñan por individual

---
+++ backend/Bookflix_Server/Bookflix_Server/Models/Reseñas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookflix_Server.Models;
+
+// Clase que representa una reseña individual
+internal class Reseña
+{
+    // Propiedades de la reseña
+    public string Autor { get; set; } // Nombre del autor de la reseña
+    public string Texto { get; set; } // Texto de la reseña
+    public int Estrellas { get; set; } // Número de estrellas otorgadas en la reseña
+    public DateTime FechaPublicacion { get; set; } // Fecha de publicación de la reseña
+}
+
+
