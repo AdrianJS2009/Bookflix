545b26ec9710d233f0d7ed9a1c289f5c570e1607
Gio <ggiomes590@g.educaand.es>
2024-11-04 20:06:07 +0100

    interfaz reseñarepository creada y reseñarepository creada y configuradas

---
+++ backend/Bookflix_Server/Bookflix_Server/Repositories/IReseñasRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bookflix_Server.Models;
+
+namespace Bookflix_Server.Repositories
+{
+    public interface IReseñasRepository
+    {
+        Task<Reseña> GetByIdAsync(int id);
+        Task AddAsync(Reseña reseña);
+        Task<bool> ReseñaExistsAsync(int id);
+        Task<IEnumerable<Reseña>> GetAllAsync();
+        Task<double> CalcularPromedioEstrellasAsync();
+    }
+}
+
+
---
+++ backend/Bookflix_Server/Bookflix_Server/Repositories/ReseñasRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookflix_Server.Models;
+
+namespace Bookflix_Server.Repositories
+{
+    public class ReseñasRepository : IReseñasRepository
+    {
+        private readonly List<Reseña> listaReseñas = new List<Reseña>();
+
+        public Task<Reseña> GetByIdAsync(int id)
+        {
+            var reseña = listaReseñas.FirstOrDefault(r => r.Id == id);
+            return Task.FromResult(reseña);
+        }
+
+        public Task AddAsync(Reseña reseña)
+        {
+            listaReseñas.Add(reseña);
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> ReseñaExistsAsync(int id)
+        {
+            var exists = listaReseñas.Any(r => r.Id == id);
+            return Task.FromResult(exists);
+        }
+
+        public Task<IEnumerable<Reseña>> GetAllAsync()
+        {
+            return Task.FromResult<IEnumerable<Reseña>>(listaReseñas);
+        }
+
+        public Task<double> CalcularPromedioEstrellasAsync()
+        {
+            if (listaReseñas.Count == 0)
+                return Task.FromResult(0.0);
+
+            var promedio = listaReseñas.Average(r => r.Estrellas);
+            return Task.FromResult(promedio);
+        }
+    }
+}
+
+
