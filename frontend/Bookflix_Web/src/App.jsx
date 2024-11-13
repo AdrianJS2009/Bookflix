347ba7c71bdc4f9511b2e240572c593b74a5c7ab
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-11-13 17:48:15 +0100

    try de encapsulación de rutas

--- frontend/Bookflix_Web/src/App.jsx
+++ frontend/Bookflix_Web/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Catalogo from "./pages/Catalogo";
 import Home from "./pages/Home";
@@ -6,23 +6,38 @@ import Login from "./pages/Login";
 import ProductoDetalle from "./pages/ProductoDetalle";
 import Registro from "./pages/Registro";
 import SobreNosotros from "./pages/SobreNosotros";
+import { AuthProvider, useAuth } from "./utils/AuthContext"; // Asegúrate de importar AuthProvider y useAuth
 
 import "./styles/default.css";
 
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" replace />;
+};
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="registro" element={<Registro />} />
-          <Route path="catalogo" element={<Catalogo />} />
-          <Route path="sobre-nosotros" element={<SobreNosotros />} />
-          <Route path="producto/:id" element={<ProductoDetalle />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="registro" element={<Registro />} />
+            <Route path="catalogo" element={<Catalogo />} />
+            <Route path="sobre-nosotros" element={<SobreNosotros />} />
+            <Route
+              path="producto/:id"
+              element={
+                <PrivateRoute>
+                  <ProductoDetalle />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
