4718090f503cf80c00f5d41738d56392b39eddd2
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-11-14 20:51:09 +0100

    dependencias de redux en el front

--- frontend/Bookflix_Web/src/App.jsx
+++ frontend/Bookflix_Web/src/App.jsx
@@ -1,35 +1,39 @@
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Carrito from "./pages/Carrito";
 import Catalogo from "./pages/Catalogo";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ProductoDetalle from "./pages/ProductoDetalle";
 import Registro from "./pages/Registro";
 import SobreNosotros from "./pages/SobreNosotros";
-import Carrito from "./pages/Carrito";
+import store from "./redux/store";
 import { AuthProvider } from "./utils/AuthContext";
 
 import "./styles/default.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      {" "}
-      {/* Coloca el AuthProvider dentro de BrowserRouter */}
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="registro" element={<Registro />} />
-            <Route path="catalogo" element={<Catalogo />} />
-            <Route path="sobre-nosotros" element={<SobreNosotros />} />
-            <Route path="producto/:id" element={<ProductoDetalle />} />
-            <Route path="carrito" element={<Carrito />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route path="registro" element={<Registro />} />
+              <Route path="catalogo" element={<Catalogo />}>
+                {/* Ruta hija para los detalles */}
+                <Route path=":productoId" element={<ProductoDetalle />} />
+              </Route>
+              <Route path="sobre-nosotros" element={<SobreNosotros />} />
+              <Route path="carrito" element={<Carrito />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
