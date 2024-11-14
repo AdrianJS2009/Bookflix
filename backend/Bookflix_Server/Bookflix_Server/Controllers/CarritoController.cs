c950af51c1913d83822f6c3f7716edbc109a3a73
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-11-14 12:01:20 +0100

    commit de rollback compilable

--- backend/Bookflix_Server/Bookflix_Server/Controllers/CarritoController.cs
+++ backend/Bookflix_Server/Bookflix_Server/Controllers/CarritoController.cs
@@ -2,9 +2,7 @@
 using Bookflix_Server.Models;
 using Bookflix_Server.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using Bookflix_Server.Data;
-using Bookflix_Server.Extensions;
+
 
 namespace Bookflix_Server.Controllers
 {
@@ -13,12 +11,10 @@ namespace Bookflix_Server.Controllers
     public class CarritoController : ControllerBase
     {
         private readonly ICarritoRepository _carritoRepository;
-        private readonly MyDbContext _context;
 
-        public CarritoController(ICarritoRepository carritoRepository, MyDbContext context)
+        public CarritoController(ICarritoRepository carritoRepository)
         {
             _carritoRepository = carritoRepository;
-            _context = context;
         }
 
         [HttpGet("{userId}")]
@@ -55,65 +51,6 @@ namespace Bookflix_Server.Controllers
         {
             await _carritoRepository.ClearCarritoAsync(userId);
             return Ok();
-        }
-
-        [HttpPost("agregar-temporal")]
-        public IActionResult AddToCarritoTemporal([FromBody] CarritoItemAgregarDto itemDto)
-        {
-            var libro = _context.Libros.Find(itemDto.LibroId);
-            if (libro == null)
-            {
-                return NotFound();
-            }
-
-            var carritoItem = new CarritoItem
-            {
-                LibroId = itemDto.LibroId,
-                Libro = libro,
-                Cantidad = itemDto.Cantidad
-            };
-
-            // Calcular el subtotal y asignarlo a una variable local
-            var subtotal = libro.Precio * itemDto.Cantidad;
-            carritoItem.GetType().GetProperty("Subtotal").SetValue(carritoItem, subtotal);
-
-            var carritoTemporal = HttpContext.Session.GetObjectFromJson<CarritoTemporal>("CarritoTemporal") ?? new CarritoTemporal();
-            carritoTemporal.Items.Add(carritoItem);
-            HttpContext.Session.SetObjectAsJson("CarritoTemporal", carritoTemporal);
-
-            return Ok();
-        }
-
-        [HttpGet("carrito-temporal")]
-        public IActionResult GetCarritoTemporal()
-        {
-            var carritoTemporal = HttpContext.Session.GetObjectFromJson<CarritoTemporal>("CarritoTemporal");
-            return carritoTemporal == null ? NotFound() : Ok(carritoTemporal);
-        }
-
-        [HttpPost("migrar-carrito/{userId}")]
-        public async Task<IActionResult> MigrateCarritoAsync(int userId)
-        {
-            var carritoTemporal = HttpContext.Session.GetObjectFromJson<CarritoTemporal>("CarritoTemporal");
-            if (carritoTemporal != null)
-            {
-                var carrito = await _carritoRepository.GetCarritoByUserIdAsync(userId) ?? new Carrito { IdUser = userId };
-                foreach (var item in carritoTemporal.Items)
-                {
-                    carrito.Items.Add(item);
-                }
-                _context.Carritos.Update(carrito);
-                await _context.SaveChangesAsync();
-                HttpContext.Session.Remove("CarritoTemporal");
-            }
-            return Ok();
-        }
-
-        [HttpGet("{userId}/total")]
-        public async Task<IActionResult> GetCarritoTotal(int userId)
-        {
-            var total = await _carritoRepository.GetCarritoTotalAsync(userId);
-            return Ok(total);
         }
     }
 }
