0154a1c4767ea3ebd4469e427564d4213e06107a
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-11-21 13:40:28 +0100

    login arreglado

--- frontend/Bookflix_Web/src/contexts/AuthContext.jsx
+++ frontend/Bookflix_Web/src/contexts/AuthContext.jsx
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
 
   const iniciarSesion = async (email, password) => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://localhost:7182/api/Auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
