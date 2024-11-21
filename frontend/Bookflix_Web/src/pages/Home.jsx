6ac66596360561d43390d1f800dde75901bc4fb0
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-11-21 11:24:22 +0100

    Update Home.jsx

--- frontend/Bookflix_Web/src/pages/Home.jsx
+++ frontend/Bookflix_Web/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import "../styles/Home.css";
+import "../styles/default.css";
+import "../styles/styles.css";
 
 const Home = () => {
   return (
