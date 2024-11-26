07d17985bd374872913ef29b1b597cb5d0f6d710
Elias Robles <149778129+erobrui2812@users.noreply.github.com>
2024-11-26 09:49:14 +0100

    Header arregliños de modal y logeo y cierre de sesion

--- frontend/Bookflix_Web/src/components/Header.jsx
+++ frontend/Bookflix_Web/src/components/Header.jsx
@@ -6,18 +6,18 @@ import classes from "./styles/Header.module.css";
 
 const Header = () => {
   const [userName, setUserName] = useState("Usuario");
-  const { items } = useCarrito(); // Usar CarritoContext para obtener los ítems del carrito
-  const { auth } = useAuth(); // Usar AuthContext para manejar el estado del usuario
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { items } = useCarrito();
+  const { auth, cerrarSesion } = useAuth();
 
   const cartCount = Array.isArray(items)
     ? items.reduce((total, item) => total + item.cantidad, 0)
     : 0;
 
   useEffect(() => {
-    const token = sessionStorage.getItem("token");
-    if (token) {
+    if (auth.token) {
       try {
-        const decoded = JSON.parse(atob(token.split(".")[1]));
+        const decoded = JSON.parse(atob(auth.token.split(".")[1])); // Decodificar el token JWT
         setUserName(
           `Hola, ${decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"]}`
         );
@@ -30,60 +30,97 @@ const Header = () => {
     }
   }, [auth]);
 
+  const toggleModal = () => setMenuOpen(!menuOpen);
+
+  const handleLogout = () => {
+    cerrarSesion();
+    setMenuOpen(false);
+    window.location.reload(); 
+  };
+  
+
   return (
-    <header className={`${classes.header} fondo-negro`}>
-      <div className={classes.navLogo}>
-        <NavLink to="/">
-          <img
-            src="/assets/logos/Logo-degradado-con bisel.png"
-            className={classes.logoNavbar}
-            alt="Bookflix logo"
-          />
-        </NavLink>
-        <div className={`${classes.userCart} texto-pequeño-bold`}>
-          <NavLink to="/login">
+    <>
+      <header className={`${classes.header} fondo-negro`}>
+        <div className={classes.navLogo}>
+          <NavLink to="/">
             <img
-              src="/assets/iconos/Logo Usuario.png"
-              className={classes.iconos}
-              alt="Icono Usuario"
+              src="/assets/logos/Logo-degradado-con bisel.png"
+              className={classes.logoNavbar}
+              alt="Bookflix logo"
             />
-            {userName}
           </NavLink>
-          <NavLink to="/carrito">
-            <img
-              src="/assets/iconos/Logo Cesta.png"
-              className={classes.iconos}
-              alt="Icono Cesta"
-            />
-            <span className={classes.cartCount}>{cartCount}</span>
-            Cesta
-          </NavLink>
+          <div className={`${classes.userCart} texto-pequeño-bold`}>
+            {auth.usuario ? (
+              <button onClick={toggleModal} className={classes.userButton}>
+                <img
+                  src="/assets/iconos/Logo Usuario.png"
+                  className={classes.iconos}
+                  alt="Icono Usuario"
+                />
+                {userName}
+              </button>
+            ) : (
+              <NavLink to="/login">
+                <img
+                  src="/assets/iconos/Logo Usuario.png"
+                  className={classes.iconos}
+                  alt="Icono Usuario"
+                />
+                {userName}
+              </NavLink>
+            )}
+            <NavLink to="/carrito">
+              <img
+                src="/assets/iconos/Logo Cesta.png"
+                className={classes.iconos}
+                alt="Icono Cesta"
+              />
+              <span className={classes.cartCount}>{cartCount}</span>
+              Cesta
+            </NavLink>
+          </div>
         </div>
-      </div>
-      <nav className={`${classes.navPrincipal} fondo-verde`}>
-        <ul className="texto-pequeño-bold texto-negro">
-          <li>
-            <a href="#novedades">Novedades</a>
-          </li>
-          <li>
-            <a href="#generos">Géneros</a>
-          </li>
-          <li>
-            <a href="#top-ventas">Top ventas</a>
-          </li>
-          <li>
-            <NavLink to="/bundles" activeClassName="active-link">
-              Bundles
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/catalogo" activeClassName="active-link">
-              Catálogo
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={`${classes.navPrincipal} fondo-verde`}>
+          <ul className="texto-pequeño-bold texto-negro">
+            <li>
+              <a href="#novedades">Novedades</a>
+            </li>
+            <li>
+              <a href="#generos">Géneros</a>
+            </li>
+            <li>
+              <a href="#top-ventas">Top ventas</a>
+            </li>
+            <li>
+              <NavLink to="/bundles" activeClassName="active-link">
+                Bundles
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/catalogo" activeClassName="active-link">
+                Catálogo
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {menuOpen && (
+        <div className={`${classes.modalOverlay} texto-pequeño`}  onClick={toggleModal}>
+          <div
+            className={classes.modalContent}
+            onClick={(e) => e.stopPropagation()} // Evitar cerrar el modal al hacer clic dentro
+          >
+            <h2 className="texto-mediano-bold">Opciones de usuario</h2>
+            <div className={classes.modalActions}>
+              <NavLink to="/perfil" activeClassName="active-link">Perfil & Pedidos</NavLink>
+              <button onClick={handleLogout}>Cerrar sesión</button>
+              <button onClick={toggleModal}>Cerrar</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
