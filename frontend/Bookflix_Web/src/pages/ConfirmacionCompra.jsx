bda696920959ec378eb84044dd60fe911980d29b
Elias Robles <149778129+erobrui2812@users.noreply.github.com>
2024-12-09 15:10:40 +0100

    Cambios a responsive Carrito, ConfirmarCompra y Producto Detalle

--- frontend/Bookflix_Web/src/pages/ConfirmacionCompra.jsx
+++ frontend/Bookflix_Web/src/pages/ConfirmacionCompra.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Button from "../components/Button";
@@ -18,14 +18,14 @@ const ConfirmacionCompra = () => {
     navigate("/catalogo");
     console.log(items)
   };
-  
+
   const totalPrecio = items.reduce(
     (sum, item) => sum + item.precio * item.cantidad,
     0
-    
+
   );
   const totalItems = items.reduce((sum, item) => sum + item.cantidad, 0);
-  
+
   return (
     <main className="confirmacion-compra-container">
       <h1 className="texto-grande">Confirmación de Compra</h1>
@@ -33,33 +33,35 @@ const ConfirmacionCompra = () => {
       <p className="texto-mediano">Detalles de la compra:</p>
       <ul className="lista-productos">
         {items.map((item, index) => (
-          <li key={index} className="producto texto-pequeño">
-            <img
-              src={item.urlImagen || "placeholder.jpg"}
-              alt={item.nombreLibro}
-              className="imagen-producto"
-            />
-            <div className="detalles-producto">
-              <p>
-                <strong>Producto:</strong> {item.nombreLibro}
-              </p>
-              <p>
-                <strong>Cantidad:</strong> {item.cantidad}
-              </p>
-              <p>
-                <strong>Precio:</strong> {(item.precio / 100).toFixed(2)} €
-              </p>
-              <p>
-                <strong>Subtotal:</strong>{" "}
-                {((item.precio * item.cantidad) / 100).toFixed(2)} €
-              </p>
-            </div>
-            <Button
-              label="Descargar Libro"
-              onClick={() => handleDescargarLibro(item.nombreLibro)}
-              styleType="btnDescargar"
-            />
-          </li>
+          <Link to={`/producto/${item.idLibro}`}>
+            <li key={index} className="producto texto-pequeño">
+              <img
+                src={item.urlImagen || "placeholder.jpg"}
+                alt={item.nombreLibro}
+                className="imagen-producto"
+              />
+              <div className="detalles-producto">
+                <p>
+                  <strong>Producto:</strong> {item.nombreLibro}
+                </p>
+                <p>
+                  <strong>Cantidad:</strong> {item.cantidad}
+                </p>
+                <p>
+                  <strong>Precio:</strong> {(item.precio / 100).toFixed(2)} €
+                </p>
+                <p>
+                  <strong>Subtotal:</strong>{" "}
+                  {((item.precio * item.cantidad) / 100).toFixed(2)} €
+                </p>
+              </div>
+              <Button
+                label="Descargar Libro"
+                onClick={() => handleDescargarLibro(item.nombreLibro)}
+                styleType="btnDescargar"
+              />
+            </li>
+          </Link>
         ))}
       </ul>
       <p className="texto-mediano">Total: {(totalPrecio / 100).toFixed(2)} €</p>
